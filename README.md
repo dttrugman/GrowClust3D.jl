fba1984adf41ebd5ce77896025bae67784cc18cb
Daniel Trugman <dtrugman@jsg.utexas.edu>
2022-03-21 16:17:54 -0500

    Updated example suite with travel time grids.
    examples/ now includes cases using nonlinloc travel time grids.

--- README.md
+++ README.md
@@ -5,21 +5,25 @@ Julia implementation of the GrowClust program for relative relocation of earthqu
 
 ` pkg> add https://github.com/dttrugman/GrowClust3D`
 
-The examples/ directory has three different julia scripts, one to run a serial version of the program without uncertainty quantification, and two parallel options with 100 bootstrap resamples: multithreaded and multiprocessing. To run these codes, navigate to this directory and run:
-
-`julia run_growclust-serial.jl example.serial.inp`
-
-or 
-
-`julia -t8 run_growclust-multithread.jl example.multithread.inp`
-
-or
-
-`julia -p8 run_growclust-multiprocess.jl example.multiprocess.inp`
-
-or
-
-
-In the second example, I specified the usage of 8 threads, while the third example uses 8 additional processors. Generally, if sufficient resources are available, multiprocessing will be faster than multithreading (especially for large datasets) due to memory considerations.
-
 [Note, to download a local copy of this repository, try `git clone https://github.com/dttrugman/GrowClust3D`.]
+
+The examples/ directory has four different julia scripts. Two of the scripts are simple examples of serial programming without bootstrapping for uncertainty quantification. One of these emulates the classic Fortran90 implementation with 1D layered velocity models, and can be used with either internal ray tracing or with pre-computed NonLinLoc travel time grids. These two example cases can be run using the commands:
+
+`julia run_growclust_classic1D.jl example.trace1D.inp`
+
+or
+
+`julia run_growclust_classic1D.jl example.grid1D.inp`.
+
+In addition, there are two parallelized examples with 100 bootstrap resamples: multithreaded and multiprocessing. To run these two examples:
+
+`julia -t8 run_growclust_multithread.jl example.multithread.inp`
+
+or
+
+`julia -p8 run_growclust_multiprocess.jl example.multiprocess.inp`
+
+or
+
+
+In the multithreaded example, I specified the use of 8 threads, while the multiprocessing example uses 8 processors. Generally, if sufficient resources are available, multiprocessing will be faster than multithreading (especially for large datasets) due to memory considerations. For most use cases, the computational overhead in transfering data to the additional processors is negligible compared to additional compute power. Note that these two parallelized versions are written for 1D velocity models but it is straightforward to convert them to 3D travel time grids using the `run_growclust_nllgrid3D.jl` as a guide.
--- examples/run_growclust_nllgrid3D.jl
+++ examples/run_growclust_nllgrid3D.jl
@@ -19,10 +19,10 @@ const distmax = 5.0          # maximum catalog(input) distance to join clusters 
 const distmax2 = 3.0         # maximum relocated distance to join clusters (km)
 const hshiftmax = 2.0        # maximum permitted horizontal cluster shifts (km)
 const vshiftmax = 2.0        # maximum permitted vertical cluster shifts (km)
-const rmedmax = Float32(0.05)# maximum median absolute tdif residual to join clusters
+const rmedmax = Float32(0.05)         # maximum median absolute tdif residual to join clusters
 const maxlink = 12            # use N best event pairs to relocate
 const nbeststa = 24           # use N best xcorr values per event pair
-const nupdate = 1000         # update progress every nupdate pairs - NEW
+const nupdate = 10000         # update progress every nupdate pairs
    
 # ------- Relative Relocation subroutine parameters -------------
 const boxwid = 3. # initial "shrinking-box" width (km)
@@ -53,7 +53,7 @@ const erad = 6371.0 # average earth radius in km, WGS-84
 ### Read Input File
 
 # read input file
-println("\nReading input file: ")
+println("\nReading input file: ",ARGS[1])
 infile_ctl = ARGS[1]
 inpD = read_gcinp(infile_ctl)
 
@@ -181,7 +181,7 @@ mean_selev = mean(sdf.selev)
     min_selev, mean_selev, max_selev)
 sta2elev = Dict(zip(sdf.sta,sdf.selev)) # map station to elevation
 
-#### Map Projection
+##### Map Projections
 
 ## setup projection
 const mapproj = inpD["proj"]
@@ -213,6 +213,11 @@ qdf[!,:qX4] .= qX4
 qdf[!,:qY4] .= qY4
 println("\nProjected event list:")
 show(qdf)
+println()
+minQX, maxQX = minimum(qdf.qX4), maximum(qdf.qX4)
+minQY, maxQY = minimum(qdf.qY4), maximum(qdf.qY4)
+@printf("min and max qX: %.4f %.4f\n", minQX, maxQX)
+@printf("min and max qY: %.4f %.4f\n", minQY, maxQY)
 println()
 
 ### Read Xcor Data
@@ -251,9 +256,6 @@ minSR, maxSR = minimum(xdf.sdist), maximum(xdf.sdist)
 @printf("min and max staX: %.4f %.4f\n", minSX, maxSX)
 @printf("min and max staY: %.4f %.4f\n", minSY, maxSY)
 @printf("min and max staR: %.4f %.4f\n", minSR, maxSR)
-
-###### Ray-Tracing Section ##########
-
 
 ######## Pre-computed NonLinLoc Grids ########
 if inpD["ttabsrc"] == "nllgrid"
@@ -673,60 +675,230 @@ end
 qrmsP = ifelse.(qndiffP.>0,sqrt.(qsseP./qndiffP),NaN64)
 qrmsS = ifelse.(qndiffS.>0,sqrt.(qsseS./qndiffS),NaN64)
 
-### Write Log / Statistics
-println("Done.\n\nRUN SUMMARY TO FOLLOW:\n")
+### Compute bootstrap statistics ###
+
+# pre-allocate: defaults are NaN for errors, 0 for nb arrays
+boot_stdH, boot_madH = fill(NaN64,nq), fill(NaN64,nq)
+boot_stdZ, boot_madZ = fill(NaN64,nq), fill(NaN64,nq)
+boot_stdT, boot_madT = fill(NaN64,nq), fill(NaN64,nq)
+boot_nbL, boot_nbM, boot_nbH = zeros(Int64,nq), zeros(Float64,nq), zeros(Int64,nq)
+
+# loop over events
+if inpD["nboot"] > 1 # need to have run bootstrapping
+
+
+    println("\nComputing bootstrap statistics...")
+    for ii = 1:nq
+
+        # nbranch statistics
+        boot_nbL[ii] = minimum(bnbM[ii,:])
+        boot_nbM[ii] = mean(bnbM[ii,:])
+        boot_nbH[ii] = maximum(bnbM[ii,:])
+        
+        # only if relocated
+        if rdf[ii,:rnb]>1
+
+            # standard errors
+            xstd = degkm*cosd(rdf[ii,:rlat])*std(blonM[ii,:])
+            ystd = degkm*std(blatM[ii,:])
+            boot_stdH[ii]=sqrt(xstd^2+ystd^2)
+            boot_stdZ[ii]=std(bdepM[ii,:])
+            boot_stdT[ii]=std(borgM[ii,:])
+
+            # MAD statistics (no division by 1/quantile(Normal(), 3/4) ≈ 1.4826)
+            xmad = degkm*cosd(rdf[ii,:rlat])*mad(blonM[ii,:],normalize=false)
+            ymad = degkm*mad(blatM[ii,:],normalize=false)
+            boot_madH[ii]=sqrt(xmad^2+ymad^2)
+            boot_madZ[ii]=mad(bdepM[ii,:],normalize=false)
+            boot_madT[ii]=mad(borgM[ii,:],normalize=false)
+
+        end
+    end
+end
+        
+#########################################################
+
+### Write Output File: Catalog
+
+println("\nWriting output catalog: ", inpD["fout_cat"])
+
+# open output file
+fcat = open(inpD["fout_cat"],"w")
+
+# loop over events
+for ii = 1:nq
+    
+    # print out origin time, relocated position, magnitude
+    dateS = Dates.format(rdf[ii,:rot],"YYYY mm dd HH MM SS.sss")
+    @printf(fcat,"%s %9d %9.5f %10.5f %7.3f %5.2f ",
+        dateS,rdf[ii,:evid],rdf[ii,:rlat],rdf[ii,:rlon],
+        rdf[ii,:rdep],qdf[ii,:qmag])
+    
+    # print out cluster number and fits
+    @printf(fcat,"%7d %7d %7d %5d %5d %5d %5.2f %5.2f ",
+        rdf[ii,:enum],rdf[ii,:rcid],rdf[ii,:rnb],qnpair[ii],
+        qndiffP[ii],qndiffS[ii],qrmsP[ii],qrmsS[ii])
+    
+    # print out uncertanties and catalog locations
+    @printf(fcat,"%7.3f %7.3f %7.3f %9.5f %10.5f %7.3f\n",
+        boot_madH[ii],boot_madZ[ii],boot_madT[ii],
+        qdf[ii,:qlat],qdf[ii,:qlon],qdf[ii,:qdep])
+end
+
+# close file
+close(fcat)
+
+
+
+### Write Output File: Cluster
+
+if !(inpD["fout_clust"] in ["none","NONE"])
+
+    println("\nWriting output clusters")
+
+    # open output file
+    fcc = open(inpD["fout_clust"],"w")
+
+    # loop over all clusters to output (only makes sense for n>=2)
+    for cc in findall(tnbranch .>= min(2,inpD["nbranch_min"]))
+        
+        # write cluster info
+        @printf(fcc,"%8d %7d %9.5f %10.5f %7.3f %7.3f\n",
+            cc,tnbranch[cc],tlats[cc],tlons[cc],tdeps[cc],torgs[cc])
+        
+        # write info for all events
+        for ii in findall(rcids.==cc)
+        
+            # print out clustering, event info, mag, otime
+            dateS = Dates.format(rdf[ii,:rot],"YYYY mm dd HH MM SS.sss")
+            @printf(fcc,"%8d %8d %9d %5.2f %s ",cc,ii,rdf[ii,:evid],qdf[ii,:qmag],dateS)
+            
+            # print event location and position w/in cluster
+            qdx = degkm*(rdf[ii,:rlon]-tlons[cc])*cosd(tlats[cc])
+            qdy = degkm*(rdf[ii,:rlat]-tlats[cc])
+            qdz = rdf[ii,:rdep]-tdeps[cc]
+            @printf(fcc,"%9.5f %10.5f %7.3f %9.4f %9.4f %9.4f ",
+                rdf[ii,:rlat],rdf[ii,:rlon],rdf[ii,:rdep],qdx,qdy,qdz)
+
+            # print out uncertanties  and catalog locations
+            @printf(fcc,"%7.3f %7.3f %7.3f %9.5f %10.5f %7.3f\n",
+                boot_madH[ii],boot_madZ[ii],boot_madT[ii],
+                qdf[ii,:qlat],qdf[ii,:qlon],qdf[ii,:qdep])
+        end
+        
+    end
+
+
+    # close file 
+    close(fcc)
+
+end
+
+### Write Output File: Bootstrapping
+
+# only if requested
+if (inpD["nboot"] > 1)&(!(inpD["fout_boot"] in ["none","NONE"]))
+    
+    println("\nWriting output bootstrapping")
+    
+    # open file
+    fbb = open(inpD["fout_boot"],"w")
+    
+    # file header
+    @printf(fbb,"%8d %5d\n",nq,inpD["nboot"])
+    
+    
+    # loop over events
+    for ii = 1:nq
+        
+        # event header
+        @printf(fbb,"%8d %9d %9.5f %10.5f %7.3f ",
+            rdf[ii,:enum],rdf[ii,:evid],rdf[ii,:rlat],rdf[ii,:rlon],rdf[ii,:rdep])
+        @printf(fbb,"%7d %7d %6.2f %7d %7d %9.5f %10.5f %7.3f ",
+            rdf[ii,:rcid],rdf[ii,:rnb],boot_nbM[ii],boot_nbL[ii],boot_nbH[ii],
+            qdf[ii,:qlat],qdf[ii,:qlon],qdf[ii,:qdep])
+        @printf(fbb,"%7.3f %7.3f %7.3f %7.3f %7.3f %7.3f\n",
+            boot_madH[ii],boot_madZ[ii],boot_madT[ii],
+            boot_stdH[ii],boot_stdZ[ii],boot_stdT[ii])
+        
+        # report each bootstrap (f90 version reports CID but this is dumb?)
+        for ib = 1:inpD["nboot"]
+            @printf(fbb,"%9.5f %10.5f %7.3f %7d\n",
+                blatM[ii,ib],blonM[ii,ib],bdepM[ii,ib],bnbM[ii,ib])
+        end
+        
+    end
+    
+    # close file
+    close(fbb) 
+    
+end
+
+
+### Write Output File: Log / Statistics
+
+println("\nWriting run log")
+
+# open log file
+flog = open(inpD["fout_log"],"w")
 
 # Run Parameters
-@printf("********************** Input Parameters ***********************\n")
-@printf("     control file:   %s\n", infile_ctl)
-@printf("       event list:   %s\n", inpD["fin_evlist"])
-@printf("     station list:   %s\n", inpD["fin_stlist"])
-@printf("     xcordat file:   %s\n", inpD["fin_xcordat"])
-@printf("  travel time src:   %s\n", inpD["ttabsrc"])
-@printf("     velocity mdl:   %s\n", inpD["fin_vzmdl"])
-@printf("   map projection:   %s %s %.6f %.6f %.2f\n", 
+@printf(flog, "********************** Input Parameters ***********************\n")
+@printf(flog, "     control file:   %s\n", infile_ctl)
+@printf(flog, "       event list:   %s\n", inpD["fin_evlist"])
+@printf(flog, "     station list:   %s\n", inpD["fin_stlist"])
+@printf(flog, "     xcordat file:   %s\n", inpD["fin_xcordat"])
+@printf(flog, "  travel time src:   %s\n", inpD["ttabsrc"])
+@printf(flog, "     velocity mdl:   %s\n", inpD["fin_vzmdl"])
+@printf(flog, "   map projection:   %s %s %.6f %.6f %.2f\n", 
     inpD["proj"], inpD["rellipse"], inpD["lon0"], inpD["lat0"], inpD["rotANG"])
-@printf("\n")
-@printf("****************** GROWCLUST Run Parameters *******************\n")
-@printf("%56s %6.2f\n", " (min rxcor value for evpair similarity coeff.): rmin =", inpD["rmin"])
-@printf("%56s %6.1f\n", " (max sta. dist for evpair similarity coeff.): delmax =", inpD["delmax"])
-@printf("%56s %6.2f\n", " (max rms residual to join clusters): rmsmax =", inpD["rmsmax"])
-@printf("%56s %6d\n" , " (num. bootstrap uncertainty iterations): nboot =", inpD["nboot"])
-@printf("\n")
-@printf("****************** Auxiliary Run Parameters *******************\n")
-@printf("%56s %6.2f\n", " max catalog dist to join clusters: ", distmax)
-@printf("%56s %6.2f\n", " max relocated dist to join clusters: ", distmax2)
-@printf("%56s %6.2f\n", " max permitted horizontal cluster shifts: ", hshiftmax)
-@printf("%56s %6.2f\n", " max permitted vertical cluster shifts: ", vshiftmax)
-@printf("%56s %6.2f\n", " max median absolute residual to join clusters: ", rmedmax)  
-@printf("***************************************************************\n")
+@printf(flog, "\n")
+@printf(flog,  "************************ Output files *************************\n")
+@printf(flog,  "     catalog file:   %s\n", inpD["fout_cat"])
+@printf(flog,  "     cluster file:   %s\n", inpD["fout_clust"])
+@printf(flog,  "         log file:   %s\n", inpD["fout_log"])
+@printf(flog,  "   bootstrap file:   %s\n", inpD["fout_boot"])
+@printf(flog, "\n")
+@printf(flog,  "****************** GROWCLUST Run Parameters *******************\n")
+@printf(flog, "%56s %6.2f\n", " (min rxcor value for evpair similarity coeff.): rmin =", inpD["rmin"])
+@printf(flog, "%56s %6.1f\n", " (max sta. dist for evpair similarity coeff.): delmax =", inpD["delmax"])
+@printf(flog, "%56s %6.2f\n", " (max rms residual to join clusters): rmsmax =", inpD["rmsmax"])
+@printf(flog, "%56s %6d\n" , " (num. bootstrap uncertainty iterations): nboot =", inpD["nboot"])
+@printf(flog, "\n")
+@printf(flog,  "****************** Auxiliary Run Parameters *******************\n")
+@printf(flog, "%56s %6.2f\n", " max catalog dist to join clusters: ", distmax)
+@printf(flog, "%56s %6.2f\n", " max relocated dist to join clusters: ", distmax2)
+@printf(flog, "%56s %6.2f\n", " max permitted horizontal cluster shifts: ", hshiftmax)
+@printf(flog, "%56s %6.2f\n", " max permitted vertical cluster shifts: ", vshiftmax)
+@printf(flog, "%56s %6.2f\n", " max median absolute residual to join clusters: ", rmedmax)  
+@printf(flog,  "***************************************************************\n")
 
 
 # Run Summary with statistics
-@printf( "\n")
-@printf( "==================================================================\n")
-@printf( "==================================================================\n")
-@printf( "\n")
-@printf( "********************  GROWCLUST Run Summary  *********************\n")
-#@printf( "\n")
-@printf( "%55s %10d\n", "Number of catalog events: ", nq)
-@printf( "%55s %10d\n", "Number of relocated events: ", nreloc)
-@printf( "%55s %10d\n", "Number of input event pairs: ", npair)
-@printf( "%55s %10d\n", "Number of event pairs used: ", sum(qnpair)/2)
-@printf( "%55s %10d\n", "Number of xcor data used (total, P+S): ", npp + nss)
-@printf( "%55s %10d\n", "Number of xcor data used (P-phase): ", npp)
-@printf( "%55s %10.4f\n", "RMS differential time residual (P-phase): ", rmsP)
-@printf( "%55s %10.4f\n", "Mean (signed) differential time residual (P-phase): ", msresP)
-@printf( "%55s %10d\n", "Number of xcor data used (S-phase): ", nss)
-@printf( "%55s %10.4f\n", "RMS differential time residual (S-phase): ", rmsS)
-@printf( "%55s %10.4f\n", "Mean (signed) differential time residual (S-phase): ", msresP)
-@printf(  "\n")
-@printf( "%55s %9d\n", "Number of clusters with >=   2 events: ", ntree2)
-@printf( "%55s %9d\n", "Number of clusters with >=   5 events: ", ntree5)
-@printf( "%55s %9d\n", "Number of clusters with >=  10 events: ", ntree10)
-@printf( "%55s %9d\n", "Number of clusters with >=  20 events: ", ntree20)
-@printf( "%55s %9d\n", "Number of clusters with >=  50 events: ", ntree50)
-@printf( "%55s %9d\n", "Number of clusters with >= 100 events: ", ntree100)
+@printf(flog, "\n")
+@printf(flog, "==================================================================\n")
+@printf(flog, "==================================================================\n")
+@printf(flog, "\n")
+@printf(flog, "********************  GROWCLUST Run Summary  *********************\n")
+#@printf(flog, "\n")
+@printf(flog, "%55s %10d\n", "Number of catalog events: ", nq)
+@printf(flog, "%55s %10d\n", "Number of relocated events: ", nreloc)
+@printf(flog, "%55s %10d\n", "Number of input event pairs: ", npair)
+@printf(flog, "%55s %10d\n", "Number of event pairs used: ", sum(qnpair)/2)
+@printf(flog, "%55s %10d\n", "Number of xcor data used (total, P+S): ", npp + nss)
+@printf(flog, "%55s %10d\n", "Number of xcor data used (P-phase): ", npp)
+@printf(flog, "%55s %10.4f\n", "RMS differential time residual (P-phase): ", rmsP)
+@printf(flog, "%55s %10.4f\n", "Mean (signed) differential time residual (P-phase): ", msresP)
+@printf(flog, "%55s %10d\n", "Number of xcor data used (S-phase): ", nss)
+@printf(flog, "%55s %10.4f\n", "RMS differential time residual (S-phase): ", rmsS)
+@printf(flog, "%55s %10.4f\n", "Mean (signed) differential time residual (S-phase): ", msresP)
+@printf(flog,  "\n")
+@printf(flog, "%55s %9d\n", "Number of clusters with >=   2 events: ", ntree2)
+@printf(flog, "%55s %9d\n", "Number of clusters with >=   5 events: ", ntree5)
+@printf(flog, "%55s %9d\n", "Number of clusters with >=  10 events: ", ntree10)
+@printf(flog, "%55s %9d\n", "Number of clusters with >=  20 events: ", ntree20)
+@printf(flog, "%55s %9d\n", "Number of clusters with >=  50 events: ", ntree50)
+@printf(flog, "%55s %9d\n", "Number of clusters with >= 100 events: ", ntree100)
    
-### Done
-println("\n\nDONE")
+# close this
+close(flog)
